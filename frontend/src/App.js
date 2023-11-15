0474f78a16f8def80d78a5c72460a09952b2dde4
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-11-14 20:52:51 -0400

    initial 22

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,39 @@
+import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
+import SignupScreen from './screens/SignupScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import Navbar from 'react-bootstrap/Navbar';
-import Badge from 'react-bootstrap/Badge';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
-import { useContext, useEffect, useState } from 'react';
-import { Store } from './Store';
-
-import SignupScreen from './screens/SignupScreen';
-
-import { getError } from './utils';
-import axios from 'axios';
 import SearchBox from './components/SearchBox';
 
 import './App.css';
 
 function App() {
-  const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { fullBox, cart, userInfo } = state;
-
-
-  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
-  const [categories, setCategories] = useState([]);
-
+  const { state } = useContext(Store);
+  const { userInfo } = state;
 
   return (
     <BrowserRouter>
-      <div
-
-      >
+      <div>
         <ToastContainer position="bottom-center" limit={1} />
         <header>
-          <Navbar >
+          <Navbar>
             <Container>
-
               <LinkContainer to="/">
                 <Navbar.Brand>
-                
-                <img
-            alt="My Pet Shop Logo"
-            src="https://www.mypetshop.co.za/wp-content/uploads/2019/11/My-petshop-LOGO.png"
-            height="70"
-            className="d-inline-block align-top"
-            />
+                  <img
+                    alt="My Pet Shop Logo"
+                    src="https://www.mypetshop.co.za/wp-content/uploads/2019/11/My-petshop-LOGO.png"
+                    height="70"
+                    className="d-inline-block align-top"
+                  />
                 </Navbar.Brand>
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -55,46 +41,42 @@ function App() {
                 <SearchBox />
                 <Nav className="me-auto  w-100  justify-content-end">
                   <img
-            alt="cart"
-            src="https://i.ibb.co/ThQrF5g/shopping-Cart-Icon-1.png"
-            height="50"
-            className="d-inline-block align-top"
-            />
-            {/* Mostrar siempre el enlace de inicio de sesión */}
-            {userInfo ? (
-  <Link className="nav-link" to="/signin">
-    <img
-      alt="signin"
-      src="https://i.ibb.co/PMQ1s9X/imagen-de-perfil.png"
-      height="50"
-      className="d-inline-block align-top"
-    />
-  </Link>
-) : (
-  <LinkContainer to="/signin">
-    <Nav.Link>
-      <img
-        alt="signin"
-        src="https://i.ibb.co/PMQ1s9X/imagen-de-perfil.png"
-        height="50"
-        className="d-inline-block align-top"
-      />
-    </Nav.Link>
-  </LinkContainer>
-)}
-          </Nav>
-        </Navbar.Collapse>
+                    alt="cart"
+                    src="https://i.ibb.co/ThQrF5g/shopping-Cart-Icon-1.png"
+                    height="50"
+                    className="d-inline-block align-top"
+                  />
+                  {userInfo ? (
+                    <Link className="nav-link" to="/signin">
+                      <img
+                        alt="signin"
+                        src="https://i.ibb.co/PMQ1s9X/imagen-de-perfil.png"
+                        height="50"
+                        className="d-inline-block align-top"
+                      />
+                    </Link>
+                  ) : (
+                    <LinkContainer to="/signin">
+                      <Nav.Link>
+                        <img
+                          alt="signin"
+                          src="https://i.ibb.co/PMQ1s9X/imagen-de-perfil.png"
+                          height="50"
+                          className="d-inline-block align-top"
+                        />
+                      </Nav.Link>
+                    </LinkContainer>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
             </Container>
           </Navbar>
         </header>
-        <div
-
-        >
+        <div>
           <Nav className="flex-column text-white w-100 p-2">
             <Nav.Item>
               <strong>Categories</strong>
             </Nav.Item>
-
           </Nav>
         </div>
         <main>
@@ -103,7 +85,6 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/signin" element={<SignupScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
-
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
