7c09b91bb78fa2c1bd4fcfcd30cae3bd6b652abe
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-15 19:33:36 -0400

    update

--- frontend/src/screens/CartScreen2.js
+++ frontend/src/screens/CartScreen2.js
@@ -1,10 +1,14 @@
-
+import { useContext } from 'react';
+import { Store } from '../Store';
 import { Helmet } from 'react-helmet-async';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MessageBox from '../components/MessageBox';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import '../Css/CartScreen.css';
 
 export default function CartScreen() {
@@ -28,26 +32,25 @@ export default function CartScreen() {
             </MessageBox>
           ) : (
             <ListGroup>
-              <ListGroup.Item>
-                <h2>Cantidad</h2> {/* Título de la sección */}
-              </ListGroup.Item>
-              {cartItems.map((item) => (
-                <ListGroup.Item key={item._id}>
-                  <div className="cart-item-container">
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className="cart-item-image img-fluid rounded img-thumbnail"
-                    />
-                    <div className="cart-item-buttons">
-                      <h3>{item.quantity}</h3> {/* Mostrar la cantidad de cada artículo */}
-                    </div>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <div className="cart-item-container">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="cart-item-image img-fluid rounded img-thumbnail"
+                  />
+                  <div className="cart-item-buttons">
+                   
+                    <span>{item.quantity}</span>{' '}
+
                   </div>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          )}
-        </Col>
+                </div>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
       </Row>
     </div>
   );
