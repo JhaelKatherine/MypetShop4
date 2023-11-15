c46607592849316d18640211c03cbcb861a0bed4
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-11-15 09:32:27 -0400

    initial 42

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,15 +12,16 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import SearchBox from './components/SearchBox';
 import CartScreen from './screens/CartScreen';
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';  // No es necesario importar useEffect y useState si no los estás utilizando
 import Badge from 'react-bootstrap/Badge';
 
 import './App.css';
 
 function App() {
-  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { state } = useContext(Store);
 
-  const { fullBox, cart, userInfo } = state;
+  const { cart } = state;
+
   return (
     <BrowserRouter>
       <div>
@@ -38,62 +39,58 @@ function App() {
                   />
                 </Navbar.Brand>
               </LinkContainer>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mx-auto" style={{ width: '100%', maxWidth: '1000px', textAlign: 'center' }}>
                 <SearchBox />
-                <Nav className="me-auto  w-100  justify-content-end">
+              </Nav>
+              <Nav className="ml-auto align-items-center">
                 <Link to="/cart" className="nav-link">
-                <img
+                  <img
                     alt="cart"
                     src="https://i.ibb.co/ThQrF5g/shopping-Cart-Icon-1.png"
                     height="50"
                     className="d-inline-block align-top"
                   />
-                    {cart.cartItems.length > 0 && (
-                      <Badge pill bg="danger">
-                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                      </Badge>
-                    )}
-                  </Link>
-
-              <LinkContainer to="/signup">
-
-              <Nav.Link>
-                <img
-                  alt="signup"
-                  src="https://i.ibb.co/PMQ1s9X/imagen-de-perfil.png"
-                  height="50"
-                  className="d-inline-block align-top"
-                />
-              </Nav.Link>
-            </LinkContainer>
-          </Nav>
-              </Navbar.Collapse>
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+                <LinkContainer to="/signup">
+                  <Nav.Link>
+                    <img
+                      alt="signup"
+                      src="https://i.ibb.co/PMQ1s9X/imagen-de-perfil.png"
+                      height="50"
+                      className="d-inline-block align-top"
+                    />
+                  </Nav.Link>
+                </LinkContainer>
+              </Nav>
             </Container>
           </Navbar>
         </header>
         <div>
           <Nav className="flex-column text-white w-100 p-2">
+            {/* Puedes agregar elementos aquí si es necesario */}
           </Nav>
         </div>
         <main>
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
-              <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SignupScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
-
-        </Container>
-      </main>
-      <footer>
-        <div className="text-center">All rights reserved</div>
-      </footer>
-    </div>
-  </BrowserRouter>
-);
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
