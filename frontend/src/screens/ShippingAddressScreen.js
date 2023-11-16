95b20278d159010c32b518f9e32c237ed38ba2be
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-11-16 12:52:40 -0400

    initial 5

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -79,50 +79,47 @@ export default function ShippingAddressScreen() {
     }
   };
   
-  const submitShippingHandler = (e) => {
-    e.preventDefault();
-  
-    if (validateForm()) {
-      ctxDispatch({
-        type: 'SAVE_SHIPPING_ADDRESS',
-        payload: {
-          fullName,
-          nit,
-          address,
-          city,
-          cellPhone,
+// ...
+
+const submitShippingHandler = (e) => {
+  e.preventDefault();
+
+  if (validateForm()) {
+    const userInformation = {
+      fullName,
+      nit,
+      address,
+      city,
+      cellPhone,
+      // Otros campos de información del usuario según sea necesario
+    };
+
+    ctxDispatch({
+      type: 'SAVE_SHIPPING_ADDRESS',
+      payload: userInformation,
+    });
+
+    localStorage.setItem('shippingAddress', JSON.stringify(userInformation));
+
+    // Aquí pasas la información del usuario a la pantalla de la factura
+    navigate('/invoice', {
+      state: {
+        invoiceData: {
+          ...userInformation,
           products: [
             // Agrega los productos según sea necesario
             { name: 'Product 1', quantity: 2, unitPrice: 10.00 },
             { name: 'Product 2', quantity: 1, unitPrice: 15.00 },
           ],
         },
-      });
-      localStorage.setItem(
-        'shippingAddress',
-        JSON.stringify({ fullName, nit, address, city, cellPhone })
-      );
-      navigate('/invoice', {
-        state: {
-          invoiceData: {
-            fullName,
-            nit,
-            address,
-            city,
-            cellPhone,
-            products: [
-              // Agrega los productos según sea necesario
-              { name: 'Product 1', quantity: 2, unitPrice: 10.00 },
-              { name: 'Product 2', quantity: 1, unitPrice: 15.00 },
-            ],
-          },
-        },
-      });
-    } else {
-      console.log('Please complete the required fields correctly.');
-    }
-  };
-  
+      },
+    });
+  } else {
+    console.log('Please complete the required fields correctly.');
+  }
+};
+
+
 
   const submitPaymentHandler = (e) => {
     e.preventDefault();
