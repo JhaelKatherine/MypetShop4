cc7e1c575456d699523b790aed23d734820367b9
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-16 04:07:45 -0400

    --

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -59,7 +59,7 @@ export default function ShippingAddressScreen() {
     }
   }, [shippingAddress, navigate]);
 
-  return (
+  /*return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
@@ -110,11 +110,9 @@ export default function ShippingAddressScreen() {
                   </div>
                 </div>
                 
-                {/* Right side for PaymentMethodScreen */}
                 <div className="col-md-6">
                   <div className="small-container">
                     <Form onSubmit={submitPaymentHandler}>
-                      {/* Payment method radio buttons */}
                       <div className='customCartScreen'>
                         <CartScreen2/>
                       </div>
@@ -156,6 +154,84 @@ export default function ShippingAddressScreen() {
                     </Form>
                   </div>
                 </div>
+              </div>
+            </div>
+          );*/
+          return (
+            <div>
+              <Helmet>
+                <title>Shipping Address</title>
+              </Helmet>
+        
+              <CheckoutSteps step1 step2></CheckoutSteps>
+              <div className="container small-container">
+                <h1 className="my-3">Shipping Address</h1>
+                <Form onSubmit={submitHandler}>
+                  <Form.Group className="mb-3" controlId="fullName">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control
+                      value={fullName}
+                      onChange={(e) => setFullName(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="address">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="city">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                      value={city}
+                      onChange={(e) => setCity(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="postalCode">
+                    <Form.Label>Postal Code</Form.Label>
+                    <Form.Control
+                      value={postalCode}
+                      onChange={(e) => setPostalCode(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="country">
+                    <Form.Label>Country</Form.Label>
+                    <Form.Control
+                      value={country}
+                      onChange={(e) => setCountry(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <div className="mb-3">
+                    <Button
+                      id="chooseOnMap"
+                      type="button"
+                      variant="light"
+                      onClick={() => navigate('/map')}
+                    >
+                      Choose Location On Map
+                    </Button>
+                    {shippingAddress.location && shippingAddress.location.lat ? (
+                      <div>
+                        LAT: {shippingAddress.location.lat}
+                        LNG:{shippingAddress.location.lng}
+                      </div>
+                    ) : (
+                      <div>No location</div>
+                    )}
+                  </div>
+        
+                  <div className="mb-3">
+                    <Button variant="primary" type="submit">
+                      Continue
+                    </Button>
+                  </div>
+                </Form>
               </div>
             </div>
           );
