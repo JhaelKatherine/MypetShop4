59df7e23f297405bedb5a233e1b41b7be8982a68
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-16 06:29:14 -0400

    --

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -6,9 +6,6 @@ import { Store } from '../Store';
 import CartScreen2 from './CartScreen2';
 import '../Css/Shipping.css';
 
-import { ListGroup } from 'react-bootstrap';
-
-
 export default function ShippingAddressScreen() {
   const navigate = useNavigate();
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -36,9 +33,9 @@ export default function ShippingAddressScreen() {
     e.preventDefault();
     ctxDispatch({
       type: 'SAVE_SHIPPING_ADDRESS',
-      payload: { fullName, nit ,address, city, cellPhone},
+      payload: { fullName, nit, address, city, cellPhone },
     });
-    localStorage.setItem('shippingAddress', JSON.stringify({ fullName, nit ,address, city, cellPhone}));
+    localStorage.setItem('shippingAddress', JSON.stringify({ fullName, nit, address, city, cellPhone }));
   };
 
   const submitPaymentHandler = (e) => {
@@ -65,110 +62,116 @@ export default function ShippingAddressScreen() {
         <div className="col-md-6">
           <div className="small-container">
             <h1 className="my-3">Billing Details</h1>
-            <p className='rojo'>Field are (*)required</p>
+            <p className='rojo'>Fields marked with (*) are required</p>
             <form onSubmit={submitPaymentHandler} className="custom-form">
-  <div className="form-group">
-    <label htmlFor="fullName">Full Name(*)</label>
-    <input
-  type="text"
-  id="fullName"
-  className="form-control"
-  value={fullName}
-  onChange={(e) => {
-    const regex = /^[A-Za-z\s]+$/;
-    const value = e.target.value;
-    if (regex.test(value) || value === '') {
-      setFullName(value);
-      e.target.setCustomValidity(''); // Restablece el mensaje de error
-    } else {
-      e.target.setCustomValidity("Please enter only letters and spaces");
-    }
-  }}
-/>
+              <div className="form-group">
+                <label htmlFor="fullName">Full Name(*)</label>
+                <input
+                  type="text"
+                  id="fullName"
+                  className="form-control"
+                  value={fullName}
+                  onChange={(e) => {
+                    const regex = /^[A-Za-z\s]+$/;
+                    const value = e.target.value;
+                    if (regex.test(value) || value === '') {
+                      setFullName(value);
+                      e.target.setCustomValidity('');
+                    } else {
+                      e.target.setCustomValidity("Please enter only letters and spaces");
+                    }
+                  }}
+                  required
+                />
+              </div>
 
-  </div>
-  
-  <div className="form-group">
-    <label htmlFor="address">Address(*)</label>
-    <input
-      type="text"
-      id="address"
-      className="form-control"
-      value={address}
-      onChange={(e) => {
-        const regex = /^[A-Za-z\s]+$/; // Expresión regular para aceptar solo letras y espacios
-        if (regex.test(e.target.value) || e.target.value === '') {
-          setCity(e.target.value);
-        } <div className="form-group">
-        <label htmlFor="nit">Nit</label>
-        <input
-          type="text"
-          id="nit"
-          className="form-control"
-          value={nit}
-          onChange={(e) => {
-            const regex = /^[0-9]+$/; // Expresión regular para aceptar solo números
-            if (regex.test(e.target.value) || e.target.value === '') {
-              setNit(e.target.value);
-            }
-          }}
-          onInvalid={(e) => e.target.setCustomValidity("Only numbers are allowed")}
-          onInput={(e) => e.target.setCustomValidity('')}
-          required
-        />
-      </div>
-      }}
-      onInvalid={(e) => e.target.setCustomValidity("This field is required")}
-      onInput={(e) => e.target.setCustomValidity('')}
-      required
-    
-      
-    />
-  </div>
-  <div className="form-group">
-    <label htmlFor="city">City(*)</label>
-    <input
-      type="text"
-      id="city"
-      className="form-control"
-      value={city}
-      onChange={(e) => {
-        const regex = /^[A-Za-z\s]+$/; // Expresión regular para aceptar solo letras y espacios
-        if (regex.test(e.target.value) || e.target.value === '') {
-          setCity(e.target.value);
-        }
-      }}
-      onInvalid={(e) => e.target.setCustomValidity("This field is required")}
-      onInput={(e) => e.target.setCustomValidity('')}
-      required
-    />
-    </div>
-    <div className="form-group">
-    <label htmlFor="cellPhone">Cell Phone(*)</label>
-    <input
-      type="text"
-      id="cellPhone"
-      className="form-control"
-      onChange={(e) => {
-        const regex = /^[0-9]+$/; // Expresión regular para aceptar solo números
-        if (regex.test(e.target.value) || e.target.value === '') {
-          setCellPhone(e.target.value);
-        }
-      }}
-      onInvalid={(e) => e.target.setCustomValidity("Only numbers are allowed")}
-      onInput={(e) => e.target.setCustomValidity('')}
-      required
-    />
-  </div>
-</form>
+              <div className="form-group">
+                <label htmlFor="address">Address(*)</label>
+                <input
+                  type="text"
+                  id="address"
+                  className="form-control"
+                  value={address}
+                  onChange={(e) => {
+                    const regex = /^[A-Za-z\s]+$/;
+                    if (regex.test(e.target.value) || e.target.value === '') {
+                      setAddress(e.target.value);
+                      e.target.setCustomValidity('');
+                    } else {
+                      e.target.setCustomValidity("Please enter only letters and spaces");
+                    }
+                  }}
+                  required
+                />
+              </div>
 
+              <div className="form-group">
+                <label htmlFor="nit">Nit</label>
+                <input
+                  type="text"
+                  id="nit"
+                  className="form-control"
+                  value={nit}
+                  onChange={(e) => {
+                    const regex = /^[0-9]+$/;
+                    if (regex.test(e.target.value) || e.target.value === '') {
+                      setNit(e.target.value);
+                      e.target.setCustomValidity('');
+                    } else {
+                      e.target.setCustomValidity("Only numbers are allowed");
+                    }
+                  }}
+                />
+              </div>
 
-                  </div>
-                </div>
-                
-                <div className="col-md-6">
-                  <div className="small-container">
-                    <Form onSubmit={submitPaymentHandler}>
+              <div className="form-group">
+                <label htmlFor="city">City(*)</label>
+                <input
+                  type="text"
+                  id="city"
+                  className="form-control"
+                  value={city}
+                  onChange={(e) => {
+                    const regex = /^[A-Za-z\s]+$/;
+                    if (regex.test(e.target.value) || e.target.value === '') {
+                      setCity(e.target.value);
+                      e.target.setCustomValidity('');
+                    } else {
+                      e.target.setCustomValidity("Please enter only letters and spaces");
+                    }
+                  }}
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="cellPhone">Cell Phone(*)</label>
+                <input
+                  type="text"
+                  id="cellPhone"
+                  className="form-control"
+                  value={cellPhone}
+                  onChange={(e) => {
+                    const regex = /^[0-9]+$/;
+                    if (regex.test(e.target.value) || e.target.value === '') {
+                      setCellPhone(e.target.value);
+                      e.target.setCustomValidity('');
+                    } else {
+                      e.target.setCustomValidity("Only numbers are allowed");
+                    }
+                  }}
+                  required
+                />
+              </div>
+
+              <Button variant="primary" type="submit">Continue</Button>
+            </form>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="small-container">
+            <CartScreen2/>
+            <Form onSubmit={submitPaymentHandler}>
                       <div className='customCartScreen'>
                         <CartScreen2/>
                       </div>
