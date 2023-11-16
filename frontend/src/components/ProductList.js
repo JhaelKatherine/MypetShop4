fee795569df033612590fd67177b420e0c66bd71
Emanuel Galindo <emanuel.galindo.9362056@usalesiana.edu.bo>
2023-11-16 00:28:01 -0400

    Addint ProductList Component

---
+++ frontend/src/components/ProductList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ProductList from '../Css/ProductList';
+
+const ProductList = ({ products }) => {
+  return (
+    <ListGroup>
+      {products.map((item) => (
+        <ListGroup.Item key={item._id}>
+          <div className="cart-item-container">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="cart-item-image img-fluid rounded img-thumbnail"
+            />
+            <div className="cart-item-description">
+              <strong>{item.name}</strong>
+              <p>{item.description}</p>
+              <p>{"$" + item.price}</p>
+            </div>
+          </div>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+};
+
+export default ProductList;
