a519feaf5efa3fada699d734241cf999c85bce1d
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-16 02:50:12 -0400

    --

--- frontend/src/screens/CartScreen2.js
+++ frontend/src/screens/CartScreen2.js
@@ -40,7 +40,7 @@ export default function CartScreen() {
   return (
     <div>
     <h1>YOUR ORDER</h1>
-    <ListGroup variant="flush" >
+    <ListGroup variant="flush" className='custom-listgroup'>
       {cartItems.length === 0 ? (
         <MessageBox>
           Cart is empty. <Link to="/">Go Shopping</Link>
