e8895ce43b007412316b94a7e98588d53bd0ae62
fundacion <emanuel.galindo.9362056@usalesiana.edu.bo>
2023-11-16 10:46:57 -0400

    Changing .js

--- frontend/src/screens/UserCardInformation.js
+++ frontend/src/screens/UserCardInformation.js
@@ -18,7 +18,6 @@ export default function UserCardInformation() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    
     const cardNumberPattern = /^[0-9]*$/;
     if (!formData.cardNumber.match(cardNumberPattern)) {
       alert('Only numbers are allowed for Card Number.');
@@ -31,80 +30,85 @@ export default function UserCardInformation() {
       return;
     }
 
-    
-
     console.log('Success', formData);
   };
 
   return (
     <div className="user-card-info-container">
-  <h1>Credit Card</h1>
-
-  <form onSubmit={handleSubmit} className="form-container">
-    <div className="form-group">
-      <label htmlFor="cardNumber">Card Number:</label>
-      <input
-        type="text"
-        id="cardNumber"
-        name="cardNumber"
-        value={formData.cardNumber}
-        onChange={handleChange}
-        pattern="[0-9]*"
-        title="Put only numbers"
-        required
-      />
-    </div>
-
-    <div className="form-group">
-      <label htmlFor="expirationDate">Expiration Date:</label>
-      <input
-        type="text"
-        id="expirationDate"
-        name="expirationDate"
-        value={formData.expirationDate}
-        onChange={handleChange}
-        pattern="(0[1-9]|1[0-2])\/?([0-9]{2})"
-        title="Only valid data : MM/YY."
-        required
-      />
-      <label htmlFor="cvv">CVV:</label>
-      <input
-        type="text"
-        id="cvv"
-        name="cvv"
-        value={formData.cvv}
-        onChange={handleChange}
-        required
-      />
-    </div>
-
-    <div className="form-group">
-      <label htmlFor="postalCode">Postal Code:</label>
-      <input
-        type="text"
-        id="postalCode"
-        name="postalCode"
-        value={formData.postalCode}
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="country">Country:</label>
-      <input
-        type="text"
-        id="country"
-        name="country"
-        value={formData.country}
-        onChange={handleChange}
-        required
-      />
-    </div>
-  </form>
-  <div className="proceed-btn-container">
-        <button type="submit" className="proceed-btn" onClick={handleSubmit}>
-          Proceed
-        </button>
+      <div className="form-section">
+        <h1>Credit Card</h1>
+        <form onSubmit={handleSubmit} className="form-container">
+          <div className="form-group">
+            <div className="form-field">
+              <label htmlFor="cardNumber">Card Number:</label>
+              <input
+                type="text"
+                id="cardNumber"
+                name="cardNumber"
+                value={formData.cardNumber}
+                onChange={handleChange}
+                pattern="[0-9]*"
+                title="Put only numbers"
+                required
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="expirationDate">Expiration Date:</label>
+              <input
+                type="text"
+                id="expirationDate"
+                name="expirationDate"
+                value={formData.expirationDate}
+                onChange={handleChange}
+                pattern="(0[1-9]|1[0-2])\/?([0-9]{2})"
+                title="Only valid data : MM/YY."
+                required
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="cvv">CVV:</label>
+              <input
+                type="text"
+                id="cvv"
+                name="cvv"
+                value={formData.cvv}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="postalCode">Postal Code:</label>
+              <input
+                type="text"
+                id="postalCode"
+                name="postalCode"
+                value={formData.postalCode}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="country">Country:</label>
+              <input
+                type="text"
+                id="country"
+                name="country"
+                value={formData.country}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
+        </form>
       </div>
-</div>
 
+      <div className="proceed-btn-section">
+        <div className="proceed-btn-container">
+          <button type="submit" className="proceed-btn" onClick={handleSubmit}>
+            Proceed
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
