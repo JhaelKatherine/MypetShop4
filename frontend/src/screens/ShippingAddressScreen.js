3bc1464c6dfb898b98e827f53c317530ac66acd2
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-16 04:30:10 -0400

    --

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -226,7 +226,7 @@ export default function ShippingAddressScreen() {
                 <h1 className="my-3">Shipping Address</h1>
                 <Form onSubmit={submitHandler}>
                 <Form.Group className="mb-3" controlId="fullName">
-                <Form.Label className="label-right">Full Name</Form.Label>
+                <Form.Label className="text-left custom-label">Full Name</Form.Label>
                 <Form.Control
                   value={fullName}
                   onChange={(e) => setFullName(e.target.value)}
