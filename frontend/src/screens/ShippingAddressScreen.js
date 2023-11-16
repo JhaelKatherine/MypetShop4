31e124c08abc56f2efe6651560ab529b299512da
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-16 04:43:24 -0400

    --

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -166,56 +166,11 @@ import { Store } from '../Store';
 import CheckoutSteps from '../components/CheckoutSteps';
 
 export default function ShippingAddressScreen() {
-  const navigate = useNavigate();
-  const { state, dispatch: ctxDispatch } = useContext(Store);
-  const {
-    fullBox,
-    userInfo,
-    cart: { shippingAddress },
-  } = state;
-  const [fullName, setFullName] = useState(shippingAddress.fullName || '');
-  const [address, setAddress] = useState(shippingAddress.address || '');
-  const [city, setCity] = useState(shippingAddress.city || '');
-  const [postalCode, setPostalCode] = useState(
-    shippingAddress.postalCode || ''
-  );
-  useEffect(() => {
-    if (!userInfo) {
-      navigate('/signin?redirect=/shipping');
-    }
-  }, [userInfo, navigate]);
-  const [country, setCountry] = useState(shippingAddress.country || '');
-  const submitHandler = (e) => {
-    e.preventDefault();
-    ctxDispatch({
-      type: 'SAVE_SHIPPING_ADDRESS',
-      payload: {
-        fullName,
-        address,
-        city,
-        postalCode,
-        country,
-        location: shippingAddress.location,
-      },
-    });
-    localStorage.setItem(
-      'shippingAddress',
-      JSON.stringify({
-        fullName,
-        address,
-        city,
-        postalCode,
-        country,
-        location: shippingAddress.location,
-      })
-    );
-    navigate('/payment');
-  };
-
-  useEffect(() => {
-    ctxDispatch({ type: 'SET_FULLBOX_OFF' });
-  }, [ctxDispatch, fullBox]);
-
+  const [name, setName] = useState('');
+  const [NIT, setNIT] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [cellPhone, setCellPhone] = useState('');
           return (
             <div>
               <Form.Group className="mb-3" controlId="fullName">
