0de45b91be5624287dd86e8d203ec2c5ca02f725
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-16 04:48:23 -0400

    --

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -157,64 +157,154 @@ export default function ShippingAddressScreen() {
               </div>
             </div>
           );*/
-          import React, { useContext, useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet-async';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom';
-import { Store } from '../Store';
-import '../Css/Shipping.css';
-
-export default function ShippingAddressScreen() {
-  const [name, setName] = useState('');
-  const [NIT, setNIT] = useState('');
-  const [address, setAddress] = useState('');
-  const [city, setCity] = useState('');
-  const [cellPhone, setCellPhone] = useState('');
-          return (
-            <div>
-              <Form.Group className="mb-3" controlId="fullName">
-  <Form.Label className="form-field-label">Full Name</Form.Label>
-  <Form.Control
-    type="text"
-    onChange={(e) => setName(e.target.value)}
-    required
-  />
-</Form.Group>
-<Form.Group className="mb-3" controlId="nit">
-  <Form.Label className="form-field-label">NIT</Form.Label>
-  <Form.Control
-    type="text"
-    onChange={(e) => setNIT(e.target.value)}
-    required
-  />
-</Form.Group>
-<Form.Group className="mb-3" controlId="address">
-  <Form.Label className="form-field-label">Address</Form.Label>
-  <Form.Control
-    type="text"
-    onChange={(e) => setAddress(e.target.value)}
-    required
-  />
-</Form.Group>
-<Form.Group className="mb-3" controlId="city">
-  <Form.Label className="form-field-label">City</Form.Label>
-  <Form.Control
-    type="text"
-    onChange={(e) => setCity(e.target.value)}
-    required
-  />
-</Form.Group>
-<Form.Group className="mb-3" controlId="cellPhone">
-  <Form.Label className="form-field-label">Cell Phone</Form.Label>
-  <Form.Control
-    type="text"
-    onChange={(e) => setCellPhone(e.target.value)}
-    required
-  />
-</Form.Group>
-
-              </div>
-            
-          );
-        }
+          import Axios from 'axios';
+          import { Link, useLocation, useNavigate } from 'react-router-dom';
+          import Container from 'react-bootstrap/Container';
+          import Form from 'react-bootstrap/Form';
+          import Button from 'react-bootstrap/Button';
+          import { Helmet } from 'react-helmet-async';
+          import { useContext, useEffect, useState } from 'react';
+          import { Store } from '../Store';
+          import { toast } from 'react-toastify';
+          import { getError } from '../utils';
+          import '../Css/AddUser.css';
+          
+          
+          export default function ShippingAddressScreen() {
+            const navigate = useNavigate();
+            const { search } = useLocation();
+          
+            const [name, setName] = useState('');
+            const [lastName, setLastName] = useState('');
+            const [userName, setUserName] = useState('');
+          
+          
+            const [email, setEmail] = useState('');
+            const [password, setPassword] = useState('');
+            const [confirmPassword, setConfirmPassword] = useState('');
+          
+            const { state, dispatch: ctxDispatch } = useContext(Store);
+            const { userInfo } = state;
+            const submitHandler = async (e) => {
+              e.preventDefault();
+              if (password !== confirmPassword) {
+                toast.error('Passwords do not match');
+                return;
+              }
+              try {
+                const { data } = await Axios.post('/api/users/signup', {
+                  name,
+                  lastName,
+                  userName,
+                  email,
+                  password,
+                });
+              } catch (err) {
+                toast.error(getError(err));
+              }
+            };
+          
+            return (
+              <>
+              <div className="blue-background"> {/* Agregar esta línea */}
+          
+                <div className="form-container">
+                <div className="centered-title">
+                   <h1>Sign Up</h1>
+                </div>
+                  <form onSubmit={submitHandler} className="custom-form">
+                    <div className="form-group">
+                      <label htmlFor="name">Name</label>
+                      <input
+                        type="text"
+                        id="name"
+                        className="form-control"
+                        onChange={(e) => setName(e.target.value)}
+                        onInvalid={(e) => e.target.setCustomValidity("This field is required")}
+                        onInput={(e) => e.target.setCustomValidity('')}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="lastName">Last Name</label>
+                      <input
+                        type="text"
+                        id="lastName"
+                        className="form-control"
+                        onChange={(e) => setLastName(e.target.value)}
+                        onInvalid={(e) => e.target.setCustomValidity("This field is required")}
+                        onInput={(e) => e.target.setCustomValidity('')}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="userName">User Name</label>
+                      <input
+                        type="text"
+                        id="userName"
+                        className="form-control"
+                        onChange={(e) => setUserName(e.target.value)}
+                        onInvalid={(e) => e.target.setCustomValidity("This field is required")}
+                        onInput={(e) => e.target.setCustomValidity('')}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="email">Email</label>
+                      <input
+                        type="email"
+                        id="email"
+                        className="form-control"
+                        onChange={(e) => setEmail(e.target.value)}
+                        onInvalid={(e) => e.target.setCustomValidity("This field is required")}
+                        onInput={(e) => e.target.setCustomValidity('')}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="password">Password</label>
+                      <input
+                        type="password"
+                        id="password"
+                        className="form-control"
+                        onChange={(e) => setPassword(e.target.value)}
+                        onInvalid={(e) => e.target.setCustomValidity("This field is required")}
+                        onInput={(e) => e.target.setCustomValidity('')}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="confirmPassword">Confirm Password</label>
+                      <input
+                        type="password"
+                        id="confirmPassword"
+                        className="form-control"
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        onInvalid={(e) => e.target.setCustomValidity("This field is required")}
+                        onInput={(e) => e.target.setCustomValidity('')}
+                        required
+                      />
+                    </div>
+                    <button className="submit" type="submit">Sign Up</button>
+                          <p className="signin">Register with</p>
+                          <div className="social-buttons-container">
+                          <button className="social-button google-button">
+                              <img src="https://static.vecteezy.com/system/resources/previews/010/353/285/original/colourful-google-logo-on-white-background-free-vector.jpg" alt="Google" />
+                              Google
+                          </button>
+                          <button className="social-button facebook-button">
+                              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook" />
+                              Facebook
+                          </button>
+                    </div>
+                    <div className="mb-3">
+                      Already have an account? <Link to="/signin" className="signin">Sign-In</Link>
+                      </div>
+                  </form>
+                </div>
+          
+                </div>
+          
+              </>
+            );
+          };
