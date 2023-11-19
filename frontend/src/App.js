7799313691fa0f3db78d4eadc7938d2adb581b69
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-11-19 19:31:36 -0400

    modify stile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Nav from 'react-bootstrap/Nav';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Store } from './Store';
 import SignupScreen from './screens/SignupScreen';
+import AddProductScreen from './screens/AddProductScreen'
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import SearchBox from './components/SearchBox';
@@ -46,6 +47,7 @@ function App() {
                 <SearchBox />
               </Nav>
               <Nav className="me-auto  w-100  justify-content-end">
+              
                 <Link to="/cart" className="nav-link">
                   <img
                     alt="cart"
@@ -98,6 +100,7 @@ function App() {
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SignupScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/addproduct" element={<AddProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
               <Route
                 path="/shipping"
