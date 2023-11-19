f45d1dd0762991434063204ff50fbcd722a22d1e
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-19 17:26:15 -0400

    creationSignIn and forgetPassword IU

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -84,9 +84,7 @@ export default function SigninScreen() {
           New customer?{' '}
           <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
         </div>
-        <div className="mb-3">
-          Forget Password? <Link to={`/forget-password`}>Reset Password</Link>
-        </div> 
+      
         </form>
       </div>
 
