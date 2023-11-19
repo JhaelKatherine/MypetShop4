006cee29299a6f5cc07518a9b8504b8e70e4524c
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-18 21:37:04 -0400

    creationSignIn and forgetPassword IU

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -10,23 +10,15 @@ import '../Css/AddUser.css';
 export default function SignipScreen() {
   const navigate = useNavigate();
   const { search } = useLocation();
-
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [userName, setUserName] = useState('');
-  const [emailExistsError, setEmailExistsError] = useState('');
-
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    if (name.trim() === '' || lastName.trim() === '' || userName.trim() === '' || email.trim() === '' || password.trim() === '' || confirmPassword.trim() === '') {
+    if (email.trim() === '' || password.trim() === '') {
       toast.error('Please complete all fields');
       return;
     }
@@ -36,17 +28,23 @@ export default function SignipScreen() {
       return;
     }
     try {
-      const { data } = await Axios.post('/api/users/signup', {
-        name,
-        lastName,
-        userName,
+      const { data } = await Axios.post('/api/users/signin', {
         email,
         password,
       });
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      navigate(redirect || '/');
     } catch (err) {
       toast.error(getError(err));
     }
   };
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
 
   return (
     <>
@@ -57,42 +55,6 @@ export default function SignipScreen() {
          <h1>Sign Up</h1>
       </div>
         <form onSubmit={submitHandler} className="custom-form">
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              className="form-control"
-              onChange={(e) => setName(e.target.value)}
-              onInvalid={(e) => e.target.setCustomValidity("This field is required")}
-              onInput={(e) => e.target.setCustomValidity('')}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              type="text"
-              id="lastName"
-              className="form-control"
-              onChange={(e) => setLastName(e.target.value)}
-              onInvalid={(e) => e.target.setCustomValidity("This field is required")}
-              onInput={(e) => e.target.setCustomValidity('')}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="userName">User Name</label>
-            <input
-              type="text"
-              id="userName"
-              className="form-control"
-              onChange={(e) => setUserName(e.target.value)}
-              onInvalid={(e) => e.target.setCustomValidity("This field is required")}
-              onInput={(e) => e.target.setCustomValidity('')}
-              required
-            />
-          </div>
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input
@@ -117,34 +79,8 @@ export default function SignipScreen() {
               required
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              className="form-control"
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              onInvalid={(e) => e.target.setCustomValidity("This field is required")}
-              onInput={(e) => e.target.setCustomValidity('')}
-              required
-            />
-          </div>
           <button className="submit" type="submit">Sign Up</button>
-                <p className="signin">Register with</p>
-                <div className="social-buttons-container">
-    <button className="social-button google-button" onClick={(e) => e.preventDefault()}>
-        <img src="https://static.vecteezy.com/system/resources/previews/010/353/285/original/colourful-google-logo-on-white-background-free-vector.jpg" alt="Google" />
-        Google
-    </button>
-    <button className="social-button facebook-button" onClick={(e) => e.preventDefault()}>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook" />
-        Facebook
-    </button>
                 
-          </div>
-          <div className="mb-3">
-            Already have an account? <Link to="/signin" className="signin">Sign-In</Link>
-            </div>
         </form>
       </div>
 
