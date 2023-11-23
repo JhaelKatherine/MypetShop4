238507c77794e0823392b1b04cbb37c714cc398e
SebastianBarra <barra20sebastian02p19@gmail.com>
2023-11-23 11:33:25 -0400

    product details and stock

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -45,7 +45,6 @@ function Product(props) {
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
-        <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>${product.price}</Card.Text>
         {product.countInStock === 0 ? (
           <Button variant="light" disabled>
