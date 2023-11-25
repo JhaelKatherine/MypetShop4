b12ab3b4d7290bfe21964401ccb9d6eddc748329
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-11-25 18:48:12 -0400

    orderhistory 5

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -1,110 +1,68 @@
-import React, { useContext, useEffect, useReducer } from 'react';
-import { Helmet } from 'react-helmet-async';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
-import { Store } from '../Store';
-import { getError } from '../utils';
-import Button from 'react-bootstrap/esm/Button';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { listOrderMine } from '../actions/orderActions';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'FETCH_REQUEST':
-      return { ...state, loading: true };
-    case 'FETCH_SUCCESS':
-      return { ...state, orders: action.payload, loading: false };
-    case 'FETCH_FAIL':
-      return { ...state, loading: false, error: action.payload };
-    default:
-      return state;
-  }
-};
+const OrderHistoryScreen = () => {
+  const dispatch = useDispatch();
 
-export default function OrderHistoryScreen() {
-  const { state } = useContext(Store);
-  const { userInfo } = state;
-  const navigate = useNavigate();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
-    loading: true,
-    error: '',
-    orders: [], // Inicializar como un array vacío
-  });
+  const orderMineList = useSelector((state) => state.orderMineList);
+  const { loading, error, orders } = orderMineList;
 
   useEffect(() => {
-    const fetchData = async () => {
-      dispatch({ type: 'FETCH_REQUEST' });
-      try {
-        const { data } = await axios.get(
-          `/api/orders/mine`,
-          { headers: { Authorization: `Bearer ${userInfo.token}` } }
-        );
-        console.log('Data from server:', data);  // Verifica la respuesta del servidor
-        dispatch({ type: 'FETCH_SUCCESS', payload: data });
-      } catch (error) {
-        dispatch({
-          type: 'FETCH_FAIL',
-          payload: getError(error),
-        });
-      }
-    };
-    fetchData();
-  }, [userInfo]);
-  
+    if (userInfo) {
+      dispatch(listOrderMine());
+    } else {
+      // Si el usuario no está autenticado, redirige al inicio de sesión
+      // Puedes personalizar esto según tus necesidades
+      // Por ejemplo: history.push('/login');
+    }
+  }, [dispatch, userInfo]);
 
+  return (
+    <div>
+      <h1>Order History</h1>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>${order.totalPrice.toFixed(2)}</td>
+                <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
+                <td>
+                  {order.isDelivered
+                    ? order.deliveredAt.substring(0, 10)
+                    : 'No'}
+                </td>
+                <td>
+                  <Link to={`/order/${order._id}`}>Details</Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 
-return (
-  <div>
-    <Helmet>
-      <title>Order History</title>
-    </Helmet>
-
-    <h1>Order History</h1>
-    {loading ? (
-      <LoadingBox></LoadingBox>
-    ) : error ? (
-      <MessageBox variant="danger">{error}</MessageBox>
-    ) : (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>DATE</th>
-            <th>TOTAL</th>
-            <th>PAID</th>
-            <th>DELIVERED</th>
-            <th>ACTIONS</th>
-          </tr>
-        </thead>
-        <tbody>
-        {orders && orders.map((order) => (
-  <tr key={order?._id ?? 'N/A'}>
-    <td>{order?.createdAt?.substring(0, 10) ?? 'N/A'}</td>
-    <td>{order?.totalPrice?.toFixed(2) ?? 'N/A'}</td>
-    <td>{order?.isPaid ? order?.paidAt?.substring(0, 10) : 'No'}</td>
-    <td>
-      {order?.isDelivered
-        ? order?.deliveredAt?.substring(0, 10)
-        : 'No'}
-    </td>
-    <td>
-      <Button
-        type="button"
-        variant="light"
-        onClick={() => {
-          navigate(`/order/${order?._id ?? 'N/A'}`);
-        }}
-      >
-        Details
-      </Button>
-    </td>
-  </tr>
-))}
-        </tbody>
-      </table>
-    )}
-  </div>
-);
-
-                }
+export default OrderHistoryScreen;
