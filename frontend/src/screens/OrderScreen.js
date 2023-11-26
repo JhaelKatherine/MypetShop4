f93c29e13173c8bd485032ad69b01628baa31608
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-26 11:42:47 -0400

    change

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -72,15 +72,53 @@ export default function OrderScreen() {
       <h1 className="my-3"> Information about your order </h1>
       <h5>ID:{orderId}</h5>
       <h5>Date:</h5>
+      <Helmet>
+        <title>Order {orderId}</title>
+      </Helmet>
+      <h1 className="my-3">Order {orderId}</h1>
       <Row>
         <Col md={8}>
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Shipping</Card.Title>
+              <Card.Text>
+                <strong>Name:</strong> {order.shippingAddress.fullName} <br />
+                <strong>Address: </strong> {order.shippingAddress.address},
+                &nbsp;
+            
+              </Card.Text>
+              {order.isDelivered ? (
+                <MessageBox variant="success">
+                  Delivered at {order.deliveredAt}
+                </MessageBox>
+              ) : (
+                <MessageBox variant="danger">Not Delivered</MessageBox>
+              )}
+            </Card.Body>
+          </Card>
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Payment</Card.Title>
+              <Card.Text>
+                <strong>Method:</strong> {order.paymentMethod}
+              </Card.Text>
+              {order.isPaid ? (
+                <MessageBox variant="success">
+                  Paid at {order.paidAt}
+                </MessageBox>
+              ) : (
+                <MessageBox variant="danger">Not Paid</MessageBox>
+              )}
+            </Card.Body>
+          </Card>
+
           <Card className="mb-3">
             <Card.Body>
               <Card.Title>Items</Card.Title>
               <ListGroup variant="flush">
                 {order.orderItems.map((item) => (
                   <ListGroup.Item key={item._id}>
-                    <Row className="align-items-center justify-content-end">
+                    <Row className="align-items-center">
                       <Col md={6}>
                         <img
                           src={item.image}
@@ -113,8 +151,14 @@ export default function OrderScreen() {
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <Row>
-                    <Col>Delivery</Col>
-                    <Col>FREE</Col>
+                    <Col>Shipping</Col>
+                    <Col>$ 70</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Tax</Col>
+                    <Col>$ 100</Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
@@ -124,10 +168,9 @@ export default function OrderScreen() {
                     </Col>
                     <Col>
                       <strong>${order.totalPrice.toFixed(2)}</strong>
-                      
                     </Col>
                   </Row>
-                </ListGroup.Item>
+                </ListGroup.Item>              
               </ListGroup>
             </Card.Body>
           </Card>
