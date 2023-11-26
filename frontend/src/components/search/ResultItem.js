9e77bba77588c2dd19dc38a9b4e61d995ab3b9a3
camilabustosvalverde <camila.bustos.7913391@usalesiana.edu.bo>
2023-11-26 01:23:29 -0400

    Button functionality fixed

--- frontend/src/components/search/ResultItem.js
+++ frontend/src/components/search/ResultItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function ResultItem({ product }) {
     return (
-        <Link to={`/products/${product._id}`} className="result-item">
+        <Link to={`/product/${product.slug}`} className="result-item">
             <span>{product.name}</span>
             <img src={product.image} alt={product.name} />
         </Link>
