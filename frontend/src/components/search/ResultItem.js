327f80b341bccec2ae3dfe7e4957c518e06377ed
camilabustosvalverde <camila.bustos.7913391@usalesiana.edu.bo>
2023-11-26 01:30:34 -0400

    Error fixed

--- frontend/src/components/search/ResultItem.js
+++ frontend/src/components/search/ResultItem.js
@@ -2,7 +2,7 @@ import '../../Css/SearchResult.css'
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function ResultItem({ product }) {
+export default function ResultItem({ product, clearResults }) {
     return (
         <Link to={`/product/${product.slug}`} className="result-item" onClick={clearResults}>
             <span>{product.name}</span>
