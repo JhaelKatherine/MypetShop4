f7c32e579d9c0ad6a1e9a04f6ddec2387774bf4c
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-26 11:28:42 -0400

    change

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -70,69 +70,68 @@ export default function OrderScreen() {
         <title>Order</title>
       </Helmet>
       <h1 className="my-3"> Information about your order </h1>
-      <h3>ID:{orderId}</h3>
-      <h3>Date:</h3>
+      <h5>ID:{orderId}</h5>
+      <h5>Date:</h5>
       <Row>
-        <Col md={8}>
-          <Card className="mb-3">
-            <Card.Body>
-              <Card.Title>Items</Card.Title>
-              <ListGroup variant="flush">
-                {order.orderItems.map((item) => (
-                  <ListGroup.Item key={item._id}>
-                    <Row className="align-items-center">
-                      <Col md={6}>
-                        <img
-                          src={item.image}
-                          alt={item.name}
-                          className="img-fluid rounded img-thumbnail"
-                        ></img>{' '}
-                        <Link to={`/product/${item.slug}`}>{item.name}</Link>
-                      </Col>
-                      <Col md={3}>
-                        <span>{item.quantity}</span>
-                      </Col>
-                      <Col md={3}>${item.price}</Col>
-                    </Row>
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4}>
-          <Card className="mb-3">
-            <Card.Body>
-              <Card.Title>Order Summary</Card.Title>
-              <ListGroup variant="flush">
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Items</Col>
-                    <Col>${order.itemsPrice}</Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Delivery</Col>
-                    <Col>FREE</Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>
-                      <strong> Order Total</strong>
-                    </Col>
-                    <Col>
-                      <strong>${order.totalPrice.toFixed(2)}</strong>
-                      
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-              </ListGroup>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+  <Col md={8}>
+    <Card className="mb-3">
+      <Card.Body>
+        <Card.Title>Items</Card.Title>
+        <ListGroup variant="flush">
+          {order.orderItems.map((item) => (
+            <ListGroup.Item key={item._id}>
+              <Row className="align-items-start"> {/* Cambié align-items-center a align-items-start */}
+                <Col md={6}>
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="img-fluid rounded img-thumbnail"
+                  ></img>{' '}
+                  <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                </Col>
+                <Col md={3}>
+                  <span>{item.quantity}</span>
+                </Col>
+                <Col md={3}>${item.price}</Col>
+              </Row>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  </Col>
+  <Col md={4}>
+    <Card className="mb-3">
+      <Card.Body>
+        <Card.Title>Order Summary</Card.Title>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            <Row>
+              <Col>Items</Col>
+              <Col>${order.itemsPrice.toFixed(2)}</Col>
+            </Row>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Row>
+              <Col>Delivery</Col>
+              <Col>FREE</Col>
+            </Row>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Row>
+              <Col>
+                <strong>Order Total</strong>
+              </Col>
+              <Col>
+                <strong>${order.totalPrice.toFixed(2)}</strong>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  </Col>
+</Row>
     </div>
   );
 }
