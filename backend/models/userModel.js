e2ab0a2d345b95aba6420c905e1eb16de2e3c6e3
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-26 19:14:30 -0400

    Register with google

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    lastName: { type: String, required: true },
-    userName: { type: String, required: true },
+    lastName: { type: String, required: false },
+    userName: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     resetToken: { type: String },
