83131334734264afa84955144385d9762abd00a6
camilabustosvalverde <camila.bustos.7913391@usalesiana.edu.bo>
2023-11-27 16:00:55 -0400

    Test

--- frontend/src/screens/Invoice.js
+++ frontend/src/screens/Invoice.js
@@ -35,71 +35,11 @@ const Invoice = () => {
         let to = email.email;
         let subject = 'Invoice';
         let html = ReactDOMServer.renderToString(
-            <div className="invoice">
-
-                <div>
-                    <h2 className={'commerce-name'}><span className={'commerce-name_aux'}>MY</span>PETSHOP</h2>
-                    <h1>Invoice</h1>
+            <>
+                <div className="invoice">
+                    {/* Resto del componente */}
                 </div>
-
-                <div>
-                    <p className={'date'}><span>Invoice ID: </span>{invoiceData._id}</p>
-                    <p className={'date'}><span>Date: </span>{new Date(invoiceData.createdAt).toLocaleDateString()}</p>
-                </div>
-
-                <div className={'invoice-bill_send'}>
-                    <div>
-                        <h3>Bill to</h3>
-                        <p>{shippingAddress.fullName}</p>
-                    </div>
-                    <div>
-                        <h3>Send to</h3>
-                        <p>{`${shippingAddress.city}, ${shippingAddress.address}`}</p>
-                    </div>
-                </div>
-
-                <div>
-                    <h3>NIT</h3>
-                    <p>{shippingAddress.nit}</p>
-                </div>
-
-                <div>
-                    <h3>Items</h3>
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Quantity</th>
-                            <th>Unit Price</th>
-                            <th>Subtotal</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {invoiceData.orderItems.map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.name}</td>
-                                <td>{item.quantity}</td>
-                                <td>Bs. {item.price}</td>
-                                <td>Bs. {item.quantity * item.price}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                </div>
-
-                <div className={'prices-invoice'}>
-
-                    <div className={'total-price'}>
-                        <h3>Tax price:</h3>
-                        <p>Bs. {invoiceData.taxPrice}</p>
-                    </div>
-                    <div className={'total-price'}>
-                        <h3>Total:</h3>
-                        <p>Bs. {invoiceData.totalPrice}</p>
-                    </div>
-
-                </div>
-            </div>
+            </>
         );
 
         let res = await axios.post('/api/send-email', { to, subject, html });
