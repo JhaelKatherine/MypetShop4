7b62eb034c63fee30bf3005289c454679ef1e72d
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-11-27 20:20:21 -0400

    filter 1

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,10 @@ import orderRouter from "./routes/orderRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
 
 dotenv.config();
-
+// "mongodb+srv://novateammypetshop:PNe5EGUIt2OHALOH@cluster0.n4sbjz1.mongodb.net/mern-amazona-app-db"
 mongoose
-  .connect(
-    "mongodb+srv://novateammypetshop:PNe5EGUIt2OHALOH@cluster0.n4sbjz1.mongodb.net/mern-amazona-app-db")
+.connect(process.env.MONGODB_URI)
+
   .then(() => {
     console.log("connected to db");
   })
