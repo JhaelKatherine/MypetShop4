1dd0420dc4b175eac4355693a41b656d66197fe5
Emanuel Galindo <emanuel.galindo.9362056@usalesiana.edu.bo>
2023-11-30 18:42:34 -0400

    Solving conflicts 1

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -87,7 +87,7 @@ export default function ProductEditScreen() {
    
   const loadBrands = async () => {
     try {
-      const response = await axios.get(`/api/brands/animal/${category}/category/${species}/brands`);
+      const response = await axios.get(`/api/brands/animal/${species}/category/${category}/brands`);
       const brandsData = response.data;
 
     setAvailableBrands(brandsData);
